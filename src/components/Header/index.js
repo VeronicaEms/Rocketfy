92dcbf4fe3bc294cffdfa471aab8f660449cdff7
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-10-08 08:45:59 -0300

    Create index.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+import logo from '../../images/corgi.PNG';
+
+export default function Header() {
+    return (
+        <Container>
+            <h1>Rocketfy</h1>
+          
+        </Container>
+    )
+}
