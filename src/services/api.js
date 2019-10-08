150297889f9d34e3e6c5d4c21af818bd36ead623
VeronicaEms <38732906+VeronicaEms@users.noreply.github.com>
2019-10-08 08:47:31 -0300

    Create api.js

---
+++ src/services/api.js
@@ -0,0 +1,97 @@
+export function loadLists() {
+    return [
+      { 
+        title: 'Tarefas', 
+        creatable: true,
+        cards: [
+          {
+            id: 1,
+            content: 'Estudar módulo 01 de NodeJS',
+            labels: ['#7159c1'],
+            user: 'https://png2.cleanpng.com/sh/2359ce462ca4e8974c81fe0652f7e39a/L0KzQYm3VsA3N5pvfZH0aYP2gLBuTgBmdZN3h91uLYfofMTvTfNwephuRdVqcnTsd7L1TgdmdKRtRdV4cnfsPbLqlP9zNZR0ReJub4DvdX77gfxscZ9sRadrMUfpRoTtgBRlbWM4RqgEN0a8QIa8UcUzQGMAT6I7M0i3Q4O1kP5o/kisspng-pembroke-welsh-corgi-cardigan-welsh-corgi-actor-co-people-talking-5b17f63fcdde23.6976905515282970238432.png'
+          },
+          {
+            id: 2,
+            content: 'Criar vídeo para o Youtube ensinando a recriar a interface do Pipefy',
+            labels: ['#7159c1'],
+            user: 'https://png2.cleanpng.com/sh/2359ce462ca4e8974c81fe0652f7e39a/L0KzQYm3VsA3N5pvfZH0aYP2gLBuTgBmdZN3h91uLYfofMTvTfNwephuRdVqcnTsd7L1TgdmdKRtRdV4cnfsPbLqlP9zNZR0ReJub4DvdX77gfxscZ9sRadrMUfpRoTtgBRlbWM4RqgEN0a8QIa8UcUzQGMAT6I7M0i3Q4O1kP5o/kisspng-pembroke-welsh-corgi-cardigan-welsh-corgi-actor-co-people-talking-5b17f63fcdde23.6976905515282970238432.png'
+          },
+          {
+            id: 3,
+            content: 'Estudar módulo 03 de React Native',
+            labels: ['#7159c1'],
+            user: 'https://png2.cleanpng.com/sh/2359ce462ca4e8974c81fe0652f7e39a/L0KzQYm3VsA3N5pvfZH0aYP2gLBuTgBmdZN3h91uLYfofMTvTfNwephuRdVqcnTsd7L1TgdmdKRtRdV4cnfsPbLqlP9zNZR0ReJub4DvdX77gfxscZ9sRadrMUfpRoTtgBRlbWM4RqgEN0a8QIa8UcUzQGMAT6I7M0i3Q4O1kP5o/kisspng-pembroke-welsh-corgi-cardigan-welsh-corgi-actor-co-people-talking-5b17f63fcdde23.6976905515282970238432.png'
+          },
+          {
+            id: 4,
+            content: 'Gravar Aula "NextJS: Utilizando server-side rendering com ReactJS"',
+            labels: ['#54e1f7'],
+            user: 'https://png2.cleanpng.com/sh/2359ce462ca4e8974c81fe0652f7e39a/L0KzQYm3VsA3N5pvfZH0aYP2gLBuTgBmdZN3h91uLYfofMTvTfNwephuRdVqcnTsd7L1TgdmdKRtRdV4cnfsPbLqlP9zNZR0ReJub4DvdX77gfxscZ9sRadrMUfpRoTtgBRlbWM4RqgEN0a8QIa8UcUzQGMAT6I7M0i3Q4O1kP5o/kisspng-pembroke-welsh-corgi-cardigan-welsh-corgi-actor-co-people-talking-5b17f63fcdde23.6976905515282970238432.png'
+          },
+          {
+            id: 5,
+            content: 'Gravar testes e deploy ReactJS',
+            labels: ['#54e1f7'],
+            user: 'https://png2.cleanpng.com/sh/2359ce462ca4e8974c81fe0652f7e39a/L0KzQYm3VsA3N5pvfZH0aYP2gLBuTgBmdZN3h91uLYfofMTvTfNwephuRdVqcnTsd7L1TgdmdKRtRdV4cnfsPbLqlP9zNZR0ReJub4DvdX77gfxscZ9sRadrMUfpRoTtgBRlbWM4RqgEN0a8QIa8UcUzQGMAT6I7M0i3Q4O1kP5o/kisspng-pembroke-welsh-corgi-cardigan-welsh-corgi-actor-co-people-talking-5b17f63fcdde23.6976905515282970238432.png'
+          },
+        ]
+      },
+      { 
+        title: 'Fazendo', 
+        creatable: false,
+        cards: [
+          {
+            id: 6,
+            content: 'Recriando clone do Pipefy',
+            labels: [],
+            user: 'https://png2.cleanpng.com/sh/2359ce462ca4e8974c81fe0652f7e39a/L0KzQYm3VsA3N5pvfZH0aYP2gLBuTgBmdZN3h91uLYfofMTvTfNwephuRdVqcnTsd7L1TgdmdKRtRdV4cnfsPbLqlP9zNZR0ReJub4DvdX77gfxscZ9sRadrMUfpRoTtgBRlbWM4RqgEN0a8QIa8UcUzQGMAT6I7M0i3Q4O1kP5o/kisspng-pembroke-welsh-corgi-cardigan-welsh-corgi-actor-co-people-talking-5b17f63fcdde23.6976905515282970238432.png'
+          }
+        ]
+      },
+      { 
+        title: 'Pausado', 
+        creatable: false,
+        cards: [
+          {
+            id: 7,
+            content: 'Gravar sobre Geolocalização e mapas com React Native',
+            labels: ['#7159c1'],
+            user: 'https://png2.cleanpng.com/sh/2359ce462ca4e8974c81fe0652f7e39a/L0KzQYm3VsA3N5pvfZH0aYP2gLBuTgBmdZN3h91uLYfofMTvTfNwephuRdVqcnTsd7L1TgdmdKRtRdV4cnfsPbLqlP9zNZR0ReJub4DvdX77gfxscZ9sRadrMUfpRoTtgBRlbWM4RqgEN0a8QIa8UcUzQGMAT6I7M0i3Q4O1kP5o/kisspng-pembroke-welsh-corgi-cardigan-welsh-corgi-actor-co-people-talking-5b17f63fcdde23.6976905515282970238432.png'
+          },
+          {
+            id: 8,
+            content: 'Gravar testes e deploy ReactJS',
+            labels: ['#54e1f7'],
+            user: 'https://png2.cleanpng.com/sh/2359ce462ca4e8974c81fe0652f7e39a/L0KzQYm3VsA3N5pvfZH0aYP2gLBuTgBmdZN3h91uLYfofMTvTfNwephuRdVqcnTsd7L1TgdmdKRtRdV4cnfsPbLqlP9zNZR0ReJub4DvdX77gfxscZ9sRadrMUfpRoTtgBRlbWM4RqgEN0a8QIa8UcUzQGMAT6I7M0i3Q4O1kP5o/kisspng-pembroke-welsh-corgi-cardigan-welsh-corgi-actor-co-people-talking-5b17f63fcdde23.6976905515282970238432.png'
+          },
+          {
+            id: 9,
+            content: 'Ajustes na biblioteca unform',
+            labels: [],
+          }
+        ]
+      },
+      { 
+        title: 'Concluído', 
+        creatable: false,
+        done: true,
+        cards: [
+          {
+            id: 10,
+            content: 'Gravar aula sobre deploy e CI com React Native',
+            labels: [],
+          },
+          {
+            id: 12,
+            content: 'Gravar testes e deploy ReactJS',
+            labels: ['#54e1f7'],
+          },
+          {
+            id: 13,
+            content: 'Gravar Aula "Internacionalização de aplicações Node.js, ReactJS e React Native"',
+            labels: ['#7159c1'],
+          }
+        ]
+      },
+    ];
+  }
